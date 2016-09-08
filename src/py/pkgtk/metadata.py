40d2a09582c00a053a2bce4f9a850e2f6a2ad922
Pierre Fernique <pfernique@gmail.com>
2016-09-08 09:26:03 +0200

    Update license headers

--- src/py/pkgtk/metadata.py
+++ src/py/pkgtk/metadata.py
@@ -1,3 +1,17 @@
+##################################################################################
+#                                                                                #
+# PkgTk: Tool kit for Python packages                                            #
+#                                                                                #
+# Homepage: http://pkgtk.readthedocs.io                                          #
+#                                                                                #
+# This software is distributed under the CeCILL-C license. You should have       #
+# received a copy of the legalcode along with this work. If not, see             #
+# <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 #
+#                                                                                #
+# File authors: Pierre Fernique <pfernique@gmail.com> (2)                        #
+#                                                                                #
+##################################################################################
+
 import copy
 
 from .config import load_config, dump_config
--- test/test_about.py
+++ test/test_about.py
@@ -1,14 +1,14 @@
 ##################################################################################
 #                                                                                #
-# PkgTk: Toolkit for packaging                                                   #
+# PkgTk: Tool kit for Python packages                                            #
 #                                                                                #
-# Homepage: pkgtk.readthedocs.io                                                 #
+# Homepage: http://pkgtk.readthedocs.io                                          #
 #                                                                                #
 # This software is distributed under the CeCILL-C license. You should have       #
 # received a copy of the legalcode along with this work. If not, see             #
 # <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 #
 #                                                                                #
-# File authors: Pierre Fernique <pfernique@gmail.com> (6)                        #
+# File authors: Pierre Fernique <pfernique@gmail.com> (2)                        #
 #                                                                                #
 ##################################################################################
 
