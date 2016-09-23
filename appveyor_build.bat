2a88e1dbd9d9bcdf744163856c8d506bbb24f639
Pierre Fernique <pfernique@gmail.com>
2016-09-23 11:29:48 +0200

    Update appveyor_build.bat

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -1,22 +1,31 @@
 SETLOCAL
 
 conda build toolchain -c statiskit
+if %errorlevel% neq 0 exit /b %errorlevel%
 conda create -n _appveyor toolchain --use-local
+if %errorlevel% neq 0 exit /b %errorlevel%
 activate _appveyor
+if %errorlevel% neq 0 exit /b %errorlevel%
 conda build libboost-python -c statiskit
+if %errorlevel% neq 0 exit /b %errorlevel%
 conda build python-scons -c statiskit
+if %errorlevel% neq 0 exit /b %errorlevel%
 conda build python-parse -c statiskit
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 IF NOT "%APPVEYOR_REPO_BRANCH%"=="master" GOTO DONE
 IF "%ANACONDA_USERNAME%"=="" GOTO DONE
 IF "%ANACONDA_PASSWORD%"=="" GOTO DONE
 
 conda install anaconda-client;
+if %errorlevel% neq 0 exit /b %errorlevel%
 anaconda login --username "%ANACONDA_USERNAME%" --password "%ANACONDA_PASSWORD%"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 FOR %%CONDA_RECIPE IN (toolchain, python-scons, python-parse) DO (
   FOR /f %%i in ('conda build %CONDA_RECIPE% --output') DO (set CONDA_FILE=%%i)
   anaconda upload --user statiskit %CONDA_FILE% --force;
+  if %errorlevel% neq 0 exit /b %errorlevel%
 )
         
 GOTO DONE
