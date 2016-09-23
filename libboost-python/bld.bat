66f2aab8ddd95451aff3232509d3d387482aecf6
Pierre Fernique <pfernique@gmail.com>
2016-09-23 13:31:48 +0200

    Update bld.bat

--- libboost-python/bld.bat
+++ libboost-python/bld.bat
@@ -1,9 +1,9 @@
 :: Start with bootstrap
-call bootstrap.bat mingw
-if errorlevel 1 exit 1
+CALL bootstrap.bat mingw
+IF errorlevel 1 exit 1
 
 :: Build step
-.\b2 install ^
+CALL b2 install ^
     --build-dir=buildboost ^
     --prefix=%LIBRARY_PREFIX% ^
     toolset=gcc ^
@@ -15,13 +15,13 @@ if errorlevel 1 exit 1
     -s ZLIB_INCLUDE="%LIBRARY_INC%" ^
     -s ZLIB_LIBPATH="%LIBRARY_LIB%" ^
     --with-python
-if errorlevel 1 exit 1
+IF errorlevel 1 exit 1
 
 :: Install fix-up for a non version-specific boost include
 move %LIBRARY_INC%\boost-1_61\boost %LIBRARY_INC%
-if errorlevel 1 exit 1
+IF errorlevel 1 exit 1
 
 :: Move dll's to LIBRARY_BIN
 move %LIBRARY_LIB%\*vc%LIB_VER%-mt-1_61.dll "%LIBRARY_BIN%"
-if errorlevel 1 exit 1
+IF errorlevel 1 exit 1
 Status API Training Shop Blog About
