386de216e6c2b861320d6375ec28eb80790b85c5
Pierre Fernique <pfernique@gmail.com>
2016-09-23 13:19:56 +0200

    Update appveyor_build.bat

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -23,12 +23,20 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 anaconda login --username "%ANACONDA_USERNAME%" --password "%ANACONDA_PASSWORD%" --hostname "AppVeyor%APPVEYOR_BUILD_NUMBER%" 
 IF %errorlevel% neq 0 exit /b %errorlevel%
 
-FOR %%CONDA_RECIPE IN (toolchain, python-scons, python-parse) DO (
-  ECHO %%CONDA_RECIPE
-  FOR /f %%i in ('conda build %%CONDA_RECIPE --output') DO (set CONDA_FILE=%%i)
-  CALL anaconda upload --user statiskit %CONDA_FILE% --force;
-  IF %errorlevel% neq 0 exit /b %errorlevel%
-)
+::FOR %%CONDA_RECIPE IN (toolchain, python-scons, python-parse) DO (
+::  ECHO %%CONDA_RECIPE
+  
+FOR /f %%i in ('conda build toolchain --output') DO (set CONDA_FILE=%%i)
+CALL anaconda upload --user statiskit %CONDA_FILE% --force;
+IF %errorlevel% neq 0 exit /b %errorlevel%
+
+FOR /f %%i in ('conda build python-scons --output') DO (set CONDA_FILE=%%i)
+CALL anaconda upload --user statiskit %CONDA_FILE% --force;
+IF %errorlevel% neq 0 exit /b %errorlevel%
+
+FOR /f %%i in ('conda build python-parse --output') DO (set CONDA_FILE=%%i)
+CALL anaconda upload --user statiskit %CONDA_FILE% --force;
+IF %errorlevel% neq 0 exit /b %errorlevel%
 
 CALL anaconda logout
 GOTO DONE
