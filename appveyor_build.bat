bf5d4008325d92442027c2e4b0c8802f194762cd
Pierre Fernique <pfernique@gmail.com>
2016-09-23 12:47:01 +0200

    Update appveyor_build.bat

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -1,17 +1,17 @@
 SETLOCAL
 
 CALL conda build toolchain -c statiskit
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% neq 0 exit /b %errorlevel%
 CALL conda create -n _appveyor toolchain --use-local
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% neq 0 exit /b %errorlevel%
 CALL activate _appveyor
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% neq 0 exit /b %errorlevel%
 :: CALL conda build libboost-python -c statiskit
 :: if %errorlevel% neq 0 exit /b %errorlevel%
 CALL conda build python-scons -c statiskit
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% neq 0 exit /b %errorlevel%
 CALL conda build python-parse -c statiskit
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% neq 0 exit /b %errorlevel%
 
 IF NOT "%APPVEYOR_REPO_BRANCH%"=="master" GOTO DONE
 IF "%ANACONDA_USERNAME%"=="" GOTO DONE
@@ -19,16 +19,18 @@ IF "%ANACONDA_PASSWORD%"=="" GOTO DONE
 
 CALL conda install -n root anaconda-client
 if %errorlevel% neq 0 exit /b %errorlevel%
+CALL anaconda logout
 CALL anaconda login --username "%ANACONDA_USERNAME%" --password "%ANACONDA_PASSWORD%"
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% neq 0 exit /b %errorlevel%
 
 FOR %%CONDA_RECIPE IN (toolchain, python-scons, python-parse) DO (
   ECHO %CONDA_RECIPE%
   FOR /f %%i in ('conda build %CONDA_RECIPE% --output') DO (set CONDA_FILE=%%i)
   CALL anaconda upload --user statiskit %CONDA_FILE% --force;
-  if %errorlevel% neq 0 exit /b %errorlevel%
+  IF %errorlevel% neq 0 exit /b %errorlevel%
 )
-        
+
+CALL anaconda logout
 GOTO DONE
 
 :DONE
