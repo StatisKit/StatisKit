bb4336056d1e18934a71bbea6e79d06771b50971
Pierre Fernique <pfernique@gmail.com>
2016-09-27 09:27:21 +0200

    Update bld.bat

--- libboost-python/bld.bat
+++ libboost-python/bld.bat
@@ -2,8 +2,11 @@
 :: cd .\tools\build
 ECHO .\bootstrap.bat
 CALL .\bootstrap.bat
-:: IF errorlevel 1 exit 1
+IF errorlevel 1 exit 1
 ECHO .\bootstrap.bat mingw
+CALL .\bootstrap.bat mingw
+ECHO type bootstrap.log
+CALL type bootstrap.log
 CALL .\bootstrap.bat mingw
 IF errorlevel 1 exit 1
 :: ECHO .\b2.exe install toolset=gcc --prefix=%SRC_DIR%\b2_for_mingw
