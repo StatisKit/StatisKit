4dd816d5df23cb69db467dd575d4626b1974d5f4
Pierre Fernique <pfernique@gmail.com>
2016-09-28 08:12:16 +0200

    Update bld.bat

--- boost_build/bld.bat
+++ boost_build/bld.bat
@@ -3,8 +3,9 @@ cd .\tools\build
 ECHO .\bootstrap.bat
 CALL .\bootstrap.bat
 IF errorlevel 1 exit 1
-ECHO .\bootstrap.bat mingw
-CALL .\bootstrap.bat mingw
+ECHO .\bootstrap.bat gcc
+CALL .\bootstrap.bat gcc
 IF errorlevel 1 exit 1
 ECHO .\b2.exe install toolset=gcc --prefix=%LIBRARY_PREFIX%
 CALL .\b2.exe install toolset=gcc --prefix=%LIBRARY_PREFIX%
+IF errorlevel 1 exit 1
