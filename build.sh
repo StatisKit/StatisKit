6b50941e2f95f8e15d648f775e2ac1133a9e66ca
Pierre Fernique <pfernique@gmail.com>
2016-09-30 13:00:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ conda build toolchain -c statiskit
 conda install toolchain --use-local -c statiskit
 source activate $CONDA_DEFAULT_ENV
 
-conda build libboost-python -c statiskit
-conda install libboost-python --use-local -c statiskit
+conda build libboost_python -c statiskit
+conda install libboost_python --use-local -c statiskit
 
 conda build python-scons -c statiskit
 conda install python-scons --use-local -c statiskit
