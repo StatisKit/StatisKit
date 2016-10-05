0709d717d50a376c59c84f96811deab57a9278f0
Pierre Fernique <pfernique@gmail.com>
2016-10-05 11:05:26 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -6,6 +6,6 @@ cd Misc/conda
 conda install python-pkgtk -c statiskit -c conda-forge
 export TOOLCHAIN=`pkgtk toolchain`
 
-for CONDA_RECIPE in libboost python-scons python-parse; do
-  conda build $CONDA_RECIPE -c statiskit
+for CONDA_RECIPE in libboost python-scons; do
+  conda build $CONDA_RECIPE -c statiskit &> /dev/null
 done
