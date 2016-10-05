bd64bdd9cdb2207b1fe7396c8d9442b89bda3af0
Pierre Fernique <pfernique@gmail.com>
2016-10-05 09:31:21 +0200

    Export TOOLCHAIN variable

--- conda/build.sh
+++ conda/build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/StatisKit/Misc.git
 cd Misc/conda
 
 conda install python-pkgtk -c statiskit -c conda-forge
-TOOLCHAIN=`pkgtk toolchain`
+export TOOLCHAIN=`pkgtk toolchain`
 
 for CONDA_RECIPE in libboost python-scons python-parse; do
   conda build $CONDA_RECIPE -c statiskit
