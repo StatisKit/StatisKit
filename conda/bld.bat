ba4bf186f35acf2d90446be378b083262500c6d7
Pierre Fernique <pfernique@gmail.com>
2016-10-05 09:27:33 +0200

    Add conda-forge channel for pkgtk

--- conda/bld.bat
+++ conda/bld.bat
@@ -6,7 +6,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cd Misc/conda
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-conda install python-pkgtk -c statiskit
+conda install python-pkgtk -c statiskit -c conda-forge
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 for /f %%i in ('pkgtk toolchain') DO (set TOOLCHAIN=%%i)
--- conda/build.sh
+++ conda/build.sh
@@ -3,7 +3,7 @@ set -xe
 git clone https://github.com/StatisKit/Misc.git
 cd Misc/conda
 
-conda install python-pkgtk -c statiskit
+conda install python-pkgtk -c statiskit -c conda-forge
 TOOLCHAIN=`pkgtk toolchain`
 
 for CONDA_RECIPE in libboost python-scons python-parse; do
