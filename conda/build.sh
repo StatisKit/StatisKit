a112c1d25ea7c143093b582d1c2de54cb9ee22ea
Pierre Fernique <pfernique@gmail.com>
2016-10-05 11:27:58 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -7,5 +7,10 @@ conda install python-pkgtk -c statiskit -c conda-forge
 export TOOLCHAIN=`pkgtk toolchain`
 
 for CONDA_RECIPE in libboost python-scons; do
-  conda build $CONDA_RECIPE -c statiskit &> /dev/null
+  conda build $CONDA_RECIPE -c statiskit &> /dev/null &
+  pid=$! # Get PID of background command
+  while kill -0 $pid; do
+    echo -n "."
+    sleep 0.5
+  done
 done
