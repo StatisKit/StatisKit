b9a5bfcef4c7c86e0704a04488634561cc7f7ff1
Pierre Fernique <pfernique@gmail.com>
2016-10-06 10:56:36 +0200

    Update upload.bat

--- conda/upload.bat
+++ conda/upload.bat
@@ -1,13 +1,15 @@
+echo OFF
+
 if "%ANACONDA_USERNAME%" == "" set /p ANACONDA_USERNAME="Username: "
 if "%ANACONDA_PASSWORD%" == "" set /p ANACONDA_USERNAME=%ANACONDA_USERNAME%%"'s password: "
-
-echo ON
 
 call conda install -n root anaconda-client
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call anaconda login --username "%ANACONDA_USERNAME%" --password "%ANACONDA_PASSWORD%" --hostname "AppVeyor%APPVEYOR_BUILD_NUMBER%"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo ON
 
 for /f %%i in ('conda build python-parse --output') DO (set CONDA_FILE=%%i)
 set errorlevel_backup=%errorlevel%
