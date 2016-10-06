d00fe43fab4aea25143a9c60833e736aa3bdc45e
Pierre Fernique <pfernique@gmail.com>
2016-10-06 10:56:10 +0200

    Update upload.sh

--- conda/upload.sh
+++ conda/upload.sh
@@ -1,10 +1,10 @@
 [[ -z $ANACONDA_USERNAME ]] && read -p "Username: " ANACONDA_USERNAME || echo "Username: "$ANACONDA_USERNAME
 [[ -z $ANACONDA_PASSWORD ]] && read -s -p %ANACONDA_USERNAME"'s password: " ANACONDA_PASSWORD ||echo %ANACONDA_USERNAME"'s password: [secure]"
 
-set -x
-
 conda install -n root anaconda-client;
 yes | anaconda login --username "$ANACONDA_USERNAME" --password "$ANACONDA_PASSWORD"
+
+set -x
 
 for CONDA_RECIPE in python-parse python-pkgtk; do
   CONDA_FILE=`conda build $CONDA_RECIPE --output`
