2d6e4d5b3a24165f60fae674a2ef557b1db5f93c
Pierre Fernique <pfernique@gmail.com>
2016-10-07 10:21:54 +0200

    Update bld.bat

--- conda/bld.bat
+++ conda/bld.bat
@@ -6,10 +6,11 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cd Misc/conda
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-git clone https://gist.github.com/c491cb08d570beeba2c417826a50a9c3.git config-toolchain
-cd config-toolchain
+git clone https://gist.github.com/c491cb08d570beeba2c417826a50a9c3.git toolchain
+cd toolchain
 call config.bat
 cd ..
+rmdir toolchain /s /q
 
 conda build libboost -c statiskit -c conda-forge
 if %errorlevel% neq 0 exit /b %errorlevel%
