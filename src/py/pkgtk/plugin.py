8caeb9e30bbe7c87ab94ad64aacb056119875eb5
Pierre Fernique <pfernique@gmail.com>
2016-10-11 10:25:22 +0200

    Update plugin.py

--- src/py/pkgtk/plugin.py
+++ src/py/pkgtk/plugin.py
@@ -15,6 +15,20 @@
 ##################################################################################
 
 import pkg_resources
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
 
 class ProxyManager(object):
 
