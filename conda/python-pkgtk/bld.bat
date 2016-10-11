0be0711ec0d801a3c9483e1d820a8091787667fc
Pierre Fernique <pfernique@gmail.com>
2016-10-11 13:14:30 +0200

    Update bld.bat

--- conda/python-pkgtk/bld.bat
+++ conda/python-pkgtk/bld.bat
@@ -1,9 +1,9 @@
 if "%PY3K%" == "1" (
   2to3 --output-dir=src/py3 -W -n src/py
-  2to3 rmdir src/py /s /q
+  rmdir src/py /s /q
   move src/py3 src/py
   2to3 --output-dir=test3 -W -n test
-  2to3 rmdir test /s /q
+  rmdir test /s /q
   move test3 test
 )
 python setup.py install
