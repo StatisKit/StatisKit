d6fac1abda51d852a2d1a2609654b49a4668d635
Pierre Fernique <pfernique@gmail.com>
2016-10-11 13:03:18 +0200

    Update build.sh

--- conda/python-pkgtk/build.sh
+++ conda/python-pkgtk/build.sh
@@ -2,5 +2,8 @@ if [[ "$PY3K" = "1" ]]; then
   2to3 --output-dir=src/py3 -W -n src/py
   rm -rf src/py
   mv src/py3 src/py
+  2to3 --output-dir=test3 -W -n test
+  rm -rf test
+  mv test3 test
 fi
 python setup.py install
