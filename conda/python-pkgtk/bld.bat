c129721b19124b245ef7bd369a479449eb02d21e
Pierre Fernique <pfernique@gmail.com>
2016-10-11 13:31:49 +0200

    Update bld.bat

--- conda/python-pkgtk/bld.bat
+++ conda/python-pkgtk/bld.bat
@@ -1,8 +1,8 @@
 echo ON
 
 if "%PY3K%" == "1" (
-  2to3 --output-dir=src/py3 -W -n src/py
-  rmdir src/py /s /q
+  2to3 --output-dir=src\py3 -W -n src\py
+  rmdir src\py /s /q
   move src\py3 src\py
   2to3 --output-dir=test3 -W -n test
   rmdir test /s /q
