bb229bb78e87b57944d3460652b30cc5bc26bbc5
Pierre Fernique <pfernique@gmail.com>
2016-10-11 10:05:18 +0200

    Update license.py

--- src/py/pkgtk/license.py
+++ src/py/pkgtk/license.py
@@ -93,7 +93,7 @@ def remove_license(filepath, delimiters):
     return content
 
 def supplementary_exclude(repository, config):
-    for key, value in config.iteritems():
+    for key, value in config.items():
         if key == 'basename':
             yield path(value).relpath(repository)
         if isinstance(value, dict):
