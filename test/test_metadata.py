bcb010ac9a6076d0e5ad79e6605e372cec7b74aa
Pierre Fernique <pfernique@gmail.com>
2016-10-11 11:32:53 +0200

    Update test_metadata.py

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -31,4 +31,4 @@ class TestMetaData(unittest.TestCase):
     """Test functions related to scripts"""
 
     def test_setup(self):
-        exec(compile(open('setup.py').read(), 'setup.py', 'exec'), globals())
+        execfile('setup.py', globals())
