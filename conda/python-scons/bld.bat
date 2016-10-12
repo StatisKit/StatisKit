076382930a24d5763167eae83049e53d870a7a0f
Pierre Fernique <pfernique@gmail.com>
2016-10-12 13:53:59 +0200

    Update bld.bat

--- conda/python-scons/bld.bat
+++ conda/python-scons/bld.bat
@@ -1,8 +1,8 @@
-"%PYTHON%" setup.py install --standard-lib
+python bootstrap.py build\scons
 if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+cd build\scons
+if errorlevel 1 exit 1
+python setup.py install --standard-lib
+if errorlevel 1 exit 1
+:: "%PYTHON%" setup.py install --standard-lib
+:: if errorlevel 1 exit 1
