5bc5201260a9be628eadd2560a04d8298250101c
Pierre Fernique <pfernique@gmail.com>
2016-10-12 21:21:51 +0200

    Update build.sh

--- conda/python-scons/build.sh
+++ conda/python-scons/build.sh
@@ -7,9 +7,6 @@ if [[ "$PY3K" = "1" ]]; then
   2to3 --output-dir=engine/SCons3 -W -n engine/SCons
   rm -rf engine/SCons
   mv engine/SCons3 engine/SCons
-  2to3 --output-dir=script3 -W -n script
-  rm -rf script
-  mv script3 script
 fi
 
 $PYTHON setup.py install --standard-lib
