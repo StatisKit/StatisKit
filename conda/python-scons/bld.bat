198b437c3f8bcb333b92e156914d32115c752fb1
Pierre Fernique <pfernique@gmail.com>
2016-10-12 14:02:48 +0200

    Update bld.bat

--- conda/python-scons/bld.bat
+++ conda/python-scons/bld.bat
@@ -1,8 +1,2 @@
-python bootstrap.py build\scons
+"%PYTHON%" setup.py install --standard-lib
 if errorlevel 1 exit 1
-cd build\scons
-if errorlevel 1 exit 1
-python setup.py install --standard-lib
-if errorlevel 1 exit 1
-:: "%PYTHON%" setup.py install --standard-lib
-:: if errorlevel 1 exit 1
