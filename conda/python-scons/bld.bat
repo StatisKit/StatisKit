231dc494c00e1369e8f868ede29be8124dedb549
Pierre Fernique <pfernique@gmail.com>
2016-10-13 09:12:30 +0200

    Update bld.bat

--- conda/python-scons/bld.bat
+++ conda/python-scons/bld.bat
@@ -4,6 +4,9 @@ if "%PY3K%" == "1" (
   2to3 --output-dir=engine/SCons3 -W -n engine/SCons
   rmdir engine/SCons /s /q
   move engine/SCons3 engine/SCons
+  2to3 -w -n scripts/scons
+  2to3 -w -n scripts/sconsign
+  2to3 -w -n scripts/scons-time
   powershell -Command "Get-Content ".\setup.py" | select -First 49"
   powershell -Command "Get-Content ".\setup.py" | select -First 49" > setup.back
   more +54 setup.py > setup.back
