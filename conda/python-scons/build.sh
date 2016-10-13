288995c6fbf97255bda2ee349466c5750cb5e61a
Pierre Fernique <pfernique@gmail.com>
2016-10-13 12:46:02 +0200

    Update build.sh

--- conda/python-scons/build.sh
+++ conda/python-scons/build.sh
@@ -6,6 +6,7 @@ if [[ "$PY3K" = "1" ]]; then
   sed -i'' -e'49,54d' setup.py
   sed -i'' -e'58,64d' script/scons
   sed -i'' -e'39d' engine/SCons/Util.py
+  sed -i'' -e's#exceptions#builtins#g' engine/SCons/Errors.py
   2to3 --output-dir=engine/SCons3 -W -n engine/SCons
   rm -rf engine/SCons
   mv engine/SCons3 engine/SCons
