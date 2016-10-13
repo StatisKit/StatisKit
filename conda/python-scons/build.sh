d8320894c9dfa6f80e91baa363f17fac85b69fb4
Pierre Fernique <pfernique@gmail.com>
2016-10-13 09:27:43 +0200

    Update build.sh

--- conda/python-scons/build.sh
+++ conda/python-scons/build.sh
@@ -4,9 +4,13 @@ set -xe
 
 if [[ "$PY3K" = "1" ]]; then
   sed -i'' -e'49,54d' setup.py
+  sed -i'' -e'58,64d' script/scons
   2to3 --output-dir=engine/SCons3 -W -n engine/SCons
   rm -rf engine/SCons
   mv engine/SCons3 engine/SCons
+  2to3 -w -n script/scons
+  2to3 -w -n script/sconsign
+  2to3 -w -n script/scons-time
 fi
 
 $PYTHON setup.py install --standard-lib
