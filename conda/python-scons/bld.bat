172c35cb46a39ffa4b425d8030cd7e06398ecfa8
Pierre Fernique <pfernique@gmail.com>
2016-10-13 08:59:15 +0200

    Update bld.bat

--- conda/python-scons/bld.bat
+++ conda/python-scons/bld.bat
@@ -1,2 +1,10 @@
+echo ON
+
+if "%PY3K%" == "1" (
+  2to3 --output-dir=engine/SCons3 -W -n engine/SCons
+  rmdir engine/SCons /s /q
+  move engine/SCons3 engine/SCons
+)
+
 "%PYTHON%" setup.py install --standard-lib
 if errorlevel 1 exit 1
