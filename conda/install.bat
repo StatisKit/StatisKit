4bc34ab33645a59571d4180cdf4b51f81e9f5ed7
Pierre Fernique <pfernique@gmail.com>
2016-10-14 15:52:53 +0200

    Update Appveyor CI

--- conda/install.bat
+++ conda/install.bat
@@ -2,12 +2,12 @@ echo OFF
 
 set DEFAULT_INSTALL_TARGETS=python-parse python-pkgtk
 
-set ANACONDA_FLAGS="-c conda-forge "%ANACONDA_FLAGS%
+set ANACONDA_FLAGS=-c conda-forge %ANACONDA_FLAGS%
 if "%ANACONDA_CHANNEL%" == "" (
-  set ANACONDA_CHANNEL="statiskit"
+  set ANACONDA_CHANNEL=statiskit
 ) else (
   echo "Using anaconda channel: "%ANACONDA_CHANNEL%
-set ANACONDA_FLAGS="-c statiskit "%ANACONDA_FLAGS%
+set ANACONDA_FLAGS=-c statiskit %ANACONDA_FLAGS%
 )
 
 if "%INSTALL_TARGETS%" == "" (
