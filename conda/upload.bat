595ac899b5553455662331e97c960f9a84520b9d
Pierre Fernique <pfernique@gmail.com>
2016-10-15 22:06:55 +0200

    Update upload.bat

--- conda/upload.bat
+++ conda/upload.bat
@@ -61,14 +61,14 @@ for %%x in (%UPLOAD_TARGETS%) do (
   <nul set /p=%ANACONDA_CHANNEL% >> _upload.bat
   <nul set /p=" " >> _upload.bat
   <nul set /p=%ANACONDA_UPLOAD_FLAGS% >> _upload.bat
-  <nul set /p=" --output') ( set UPLOAD_FILE=%%%%i )" >> _upload.bat
+  <nul set /p=" --output') set UPLOAD_FILE=%%%%i" >> _upload.bat
   echo. >> _upload.bat
   <nul set /p="anaconda upload --user " >> _upload.bat
   <nul set /p=%ANACONDA_CHANNEL% >> _upload.bat
   <nul set /p=" %%UPLOAD_FILE%%" >> _upload.bat
   echo. >> _upload.bat
   <nul set /p="if %%errorlevel%% neq 0 " >> _upload.bat
-  echo upload failed >> _upload.bat
+  echo echo upload failed >> _upload.bat
   more _upload.bat
   call _upload.bat
   del _upload.bat
