9db2b1c76dbc2914c0a264ad541dccbacce1c8c2
Pierre Fernique <pfernique@gmail.com>
2016-10-20 15:37:56 +0200

    Downgrade tk

--- conda/tk/bld.bat
+++ conda/tk/bld.bat
@@ -4,8 +4,12 @@ if "%ARCH%"=="32" (
   set MACHINE="AMD64"
 )
 
-curl -L -o tcl%PKG_VERSION%.tar.gz "ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tcl%PKG_VERSION%-src.tar.gz"
-curl -L -o tk%PKG_VERSION%.tar.gz "ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk%PKG_VERSION%-src.tar.gz"
+:: Get the major minor version info (e.g. `8_5`) so it can be used in the URL.
+python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
+set /p MAJ_MIN_VER=<temp.txt
+
+curl -L -o tcl%PKG_VERSION%.tar.gz "ftp://ftp.tcl.tk/pub/tcl/tcl%MAJ_MIN_VER%/tcl%PKG_VERSION%-src.tar.gz"
+curl -L -o tk%PKG_VERSION%.tar.gz "ftp://ftp.tcl.tk/pub/tcl/tcl%MAJ_MIN_VER%/tk%PKG_VERSION%-src.tar.gz"
 
 7za x -so tcl%PKG_VERSION%.tar.gz | 7za x -si -aoa -ttar
 7za x -so tk%PKG_VERSION%.tar.gz | 7za x -si -aoa -ttar
@@ -19,14 +23,14 @@ REM If you don't have this path, you are missing the Windows 7 SDK.  Please inst
 REM   NOTE: Later SDKs remove tmschema.h.  It really is necessary to use the Win 7 SDK.
 set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
 
-:: Tk build
 
 cd ..\..\tk%PKG_VERSION%\win
 nmake -f makefile.vc all install INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\tcl%PKG_VERSION%
 if %ERRORLEVEL% GTR 0 exit 1
 
-:: Make sure that `wish` can be called without the version info.
-copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish.exe
+:: Change the major minor version info so that it can be used with the exe.
+python -c "import os; print(''.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
+set /p MAJ_MIN_VER=<temp.txt
 
-:: No `t` version of wish86.exe
-copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish86.exe
+:: Make sure that `wish` can be called without the version info.
+copy %LIBRARY_PREFIX%\bin\wish%MAJ_MIN_VER%.exe %LIBRARY_PREFIX%\bin\wish.exe
