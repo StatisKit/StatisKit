edafe1957eb92ed68671d63277cf57d8c50dfe5b
Pierre Fernique <pfernique@gmail.com>
2016-10-21 14:43:34 +0200

    Update CIs and scripts

--- conda/install.bat
+++ conda/install.bat
@@ -1,26 +1,37 @@
 echo OFF
 
-set DEFAULT_INSTALL_TARGETS=python-parse python-pkgtk
+set DEFAULT_ANACONDA_INSTALL_RECIPES=python-pkgtk
+set DEFAULT_ANACONDA_CHANNELS=statiskit conda-forge
+set DEFAULT_ANACONDA_INSTALL_FLAGS=--use-local
 
-set ANACONDA_INSTALL_FLAGS=-c conda-forge %ANACONDA_INSTALL_FLAGS%
-if "%ANACONDA_CHANNEL%" == "" (
-  set ANACONDA_CHANNEL=statiskit
+if "%ANACONDA_CHANNELS%" == "" (
+    set ANACONDA_CHANNELS=%DEFAULT_ANACONDA_CHANNELS%
 ) else (
-  echo Using anaconda channel: %ANACONDA_CHANNEL%
-set ANACONDA_INSTALL_FLAGS=-c statiskit %ANACONDA_INSTALL_FLAGS%
+    echo Channels used: %ANACONDA_CHANNELS%
 )
 
-if "%INSTALL_TARGETS%" == "" (
-  set INSTALL_TARGETS=%DEFAULT_INSTALL_TARGETS%
-) else (  
-  echo "Targets to install: "%INSTALL_TARGETS%
+set ANACONDA_CHANNEL_FLAGS=
+for %%ANACONDA_CHANNEL_FLAG in (%ANACONDA_CHANNELS%) do (
+    set ANACONDA_CHANNEL_FLAGS=%ANACONDA_CHANNEL_FLAGS% -c %%ANACONDA_CHANNEL_FLAG
+)
+
+if "%ANACONDA_INSTALL_RECIPES%" == "" (
+    set ANACONDA_INSTALL_RECIPES=%DEFAULT_ANACONDA_INSTALL_RECIPES%
+) else (
+    echo Recipes to build: %ANACONDA_INSTALL_RECIPES%
+)
+
+if "%ANACONDA_INSTALL_FLAGS%" == "" (
+    set ANACONDA_INSTALL_FLAGS=%DEFAULT_ANACONDA_INSTALL_FLAGS%
 )
 
 echo ON
 
-for %%x in (%INSTALL_TARGETS%) do (
-  conda install %%x --use-local -c %ANACONDA_CHANNEL% %ANACONDA_INSTALL_FLAGS%
-  if %errorlevel% neq 0 (
-    exit /b %errorlevel%
-  )
+for %%i in (%ANACONDA_INSTALL_RECIPES%) do (
+    conda install %%i --use-local %ANACONDA_CHANNEL_FLAGS% %ANACONDA_INSTALL_FLAGS%
+    if errorlevel 1 (
+        exit /b 1
+    )
 )
+
+ECHO OFF
