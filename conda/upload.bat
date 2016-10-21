42bc4ad405e484ba66e6428a1f9e2361e2306f49
Pierre Fernique <pfernique@gmail.com>
2016-10-21 13:56:35 +0200

    Correct !errorlevel!

--- conda/upload.bat
+++ conda/upload.bat
@@ -30,44 +30,44 @@ if "%ANACONDA_BUILD_RECIPES%" == "" (
 if "%ANACONDA_USERNAME%" == "" (
     set /p ANACONDA_USERNAME="Username: "
 ) else (
-    echo Username: %ANACONDA_USERNAME%;
+    echo Username: %ANACONDA_USERNAME%
 )
 
 if "%ANACONDA_PASSWORD%" == "" (
     set /p ANACONDA_USERNAME=%ANACONDA_USERNAME%%"'s password: "
 ) else (
-    echo %ANACONDA_USERNAME%'s password: [secure];
+    echo %ANACONDA_USERNAME%'s password: [secure]
 )
 
 echo ON
 
 conda install -n root anaconda-client
-if errorlevel neq 0 (
-    exit /b errorlevel
+if !errorlevel! neq 0 (
+    exit /b !errorlevel!
 )
 
 echo OFF
 
 echo y|anaconda login --username %ANACONDA_USERNAME% --password %ANACONDA_PASSWORD%
-if errorlevel neq 0 (
-    exit /b errorlevel
+if !errorlevel! neq 0 (
+    exit /b !errorlevel!
 )
 
 echo ON
 
 if "%TOOLCHAIN%" == "" (
     git clone https://gist.github.com/c491cb08d570beeba2c417826a50a9c3.git toolchain
-    if errorlevel neq 0 (
+    if !errorlevel! neq 0 (
         anaconda logout
-        exit /b errorlevel
+        exit /b !errorlevel!
     )
     cd toolchain
     call config.bat
-    if errorlevel neq 0 (
+    if !errorlevel! neq 0 (
         cd ..
         anaconda logout
         rmdir toolchain /s /q
-        exit /b errorlevel
+        exit /b !errorlevel!
     )
     cd ..
     rmdir toolchain /s /q
