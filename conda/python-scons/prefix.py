0754a2b77074a24c0a6939210873f0fd4f5fb9a7
Pierre Fernique <pfernique@gmail.com>
2017-01-15 17:03:06 +0100

    Update prefix.py

--- conda/python-scons/prefix.py
+++ conda/python-scons/prefix.py
@@ -1,16 +1,17 @@
 import sys
 from SCons.Script import AddOption, GetOption
 
+AddOption('--prefix',
+          dest    = 'prefix',
+          type    = 'string',
+          nargs   = 1,
+          action  = 'store',
+          metavar = 'DIR',
+          help    = 'installation prefix',
+          default = sys.prefix)
+    
 def generate(env, **kwargs):
     env['PREFIX'] = GetOption('prefix')
 
 def exists(env):
-    AddOption('--prefix',
-              dest    = 'prefix',
-              type    = 'string',
-              nargs   = 1,
-              action  = 'store',
-              metavar = 'DIR',
-              help    = 'installation prefix',
-              default = sys.prefix)
     return 1
