4ce0039e34f4f22ff3770555e8c775e8bc279c5f
Pierre Fernique <pfernique@gmail.com>
2017-01-15 12:10:11 +0100

    Update bld.bat

--- conda/python-scons/bld.bat
+++ conda/python-scons/bld.bat
@@ -2,3 +2,23 @@ echo ON
 
 "%PYTHON%" setup.py install --standard-lib
 if errorlevel 1 exit 1
+
+if not exist %PREFIX%/etc/conda/activate.d mkdir %PREFIX%/etc/conda/activate.d
+if errorlevel 1 exit 1
+if not exist %PREFIX%/etc/conda/deactivate.d mkdir -p $PREFIX/etc/conda/dedeactivate.d
+type NUL > %PREFIX%/etc/conda/activate.d/scons_vars.bat
+if errorlevel 1 exit 1
+type NUL > %PREFIX%/etc/conda/deactivate.d/scons_vars.bat
+if errorlevel 1 exit 1
+
+echo set SCONSFLAGS="--sitedir=%CONDA_PREFIX%/share/site_scons" > %PREFIX%/etc/conda/activate.d/scons_vars.bat
+if errorlevel 1 exit 1
+echo set SCONSFLAGS= > %PREFIX%/etc/conda/deactivate.d/scons_vars.bat
+if errorlevel 1 exit 1
+
+if not exist %PREFIX%/share/site_scons mkdir %PREFIX%/share/site_scons
+if errorlevel 1 exit 1
+if not exist %PREFIX%/share/site_scons/site_init.py type NUL > %PREFIX%/share/site_scons/site_init.py
+if errorlevel 1 exit 1
+if not exist %PREFIX%/share/site_scons/site_tools mkdir %PREFIX%/share/site_scons/site_tools
+if errorlevel 1 exit 1
