1b6fa3f1cfe7a8c95a2a8db5c08df1575690eebd
Pierre Fernique <pfernique@gmail.com>
2017-01-16 12:25:01 +0100

    Correct Windows site_scons

--- conda/python-scons/scons_vars.bat
+++ conda/python-scons/scons_vars.bat
@@ -1,3 +1,4 @@
+set BLABLA="ABC"
 for /f "delims=" %i in ('python -c "import os; print os.environ.get('CONDA_PREFIX').replace('/', '\\')"') do set TEMP_PREFIX=%i
 echo %TEMP_PREFIX%
 set SCONSFLAGS="--site-dir=%TEMP_PREFIX%/share/site_scons"
