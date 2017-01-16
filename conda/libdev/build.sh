63afbee2601418a26f7e1096c06b784b5c146b7a
Pierre Fernique <pfernique@gmail.com>
2017-01-16 10:27:08 +0100

    Test python-scons

--- conda/libdev/build.sh
+++ conda/libdev/build.sh
@@ -39,5 +39,4 @@ if [[ "$__GNUC__" -ne  "True" ]]; then
 fi
 
 mv $RECIPE_DIR/toolchains.py $PREFIX/share/site_scons/site_tools/toolchains.py
-mv $RECIPE_DIR/python.py $PREFIX/share/site_scons/site_tools/python.py
 mv $RECIPE_DIR/boost_python.py $PREFIX/share/site_scons/site_tools/boost_python.py
