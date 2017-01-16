9967966f28be4e1d3f033b340a28903c138b5f82
Pierre Fernique <pfernique@gmail.com>
2017-01-16 15:35:17 +0100

    Correct paths for windows

--- conda/libdev/bld.bat
+++ conda/libdev/bld.bat
@@ -1,2 +1,2 @@
-move %RECIPE_DIR%/toolchains.py %PREFIX%/share/site_scons/site_tools/toolchains.py
-move %RECIPE_DIR%/boost_python.py %PREFIX%/share/site_scons/site_tools/boost_python.py
+move %RECIPE_DIR%\toolchains.py %PREFIX%\share\site_scons\site_tools\toolchains.py
+move %RECIPE_DIR%\boost_python.py %PREFIX%\share\site_scons\site_tools\boost_python.py
