96fd4e9a71727b7564b1250b597864fdfd27ce46
Pierre Fernique <pfernique@gmail.com>
2017-01-17 10:52:56 +0100

    Add system tool

---
+++ conda/python-scons/system.py
@@ -0,0 +1,28 @@
+import platform
+from SCons.Script import AddOption, GetOption
+
+SYSTEMS = dict(Linux   = "linux",
+	           Darwin  = "osx",
+	           Windows = "windows")
+
+
+system = platform.system()
+if not system in SYSTEMS:
+	system = "unknown"
+
+AddOption('--system',
+          dest    = 'system',
+          type    = 'string',
+          nargs   = 1,
+          action  = 'store',
+          help    = 'system',
+          choices = SYSTEMS.values(),
+          default = system)
+    
+def generate(env, **kwargs):
+    env['SYSTEM'] = GetOption('system')
+    if env['SYSTEM'] == 'unknown':
+    	raise ValueError('Unknown system')
+
+def exists(env):
+    return 1
