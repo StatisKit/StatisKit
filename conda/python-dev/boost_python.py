8c65c21982f5632ab5110f969c56f5c33f9d11ad
Pierre Fernique <pfernique@gmail.com>
2017-01-18 11:38:02 +0100

    Modify SCons tools

--- conda/python-dev/boost_python.py
+++ conda/python-dev/boost_python.py
@@ -19,7 +19,7 @@ def generate(env):
             if SYSTEM == 'linux' and len(sources) == 1:
                 cmd = env.Command(sources[0].target_from_source('', '.h.gch'), sources[0], '$CXX -o $TARGET -x c++-header -c -fPIC $SHCXXFLAGS $_CCCOMCOM $SOURCE')
                 env.Depends(targets, cmd)
-
+            env.Depends(target, targets)
             source = env.File('response_file.rsp')
             with open(source.abspath, 'w') as filehandler:
                 filehandler.write(' '.join(target.abspath.replace('\\','/') + ' ' for target in targets))
