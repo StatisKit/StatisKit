eef4d576d62c8970c4a2854203e3ee7a51a29da8
Pierre Fernique <pfernique@gmail.com>
2017-01-18 10:17:35 +0100

    Modify SCons tools

---
+++ conda/python-dev/boost_python.py
@@ -0,0 +1,40 @@
+from types import MethodType
+
+def generate(env):
+    """Add Builders and construction variables to the Environment."""
+    
+    if not 'boost_python' in env['TOOLS'][:-1]:
+        env.Tool('system')
+        env.AppendUnique(LIBS = ['boost_python'])
+        env.AppendUnique(CPPDEFINES = ['BOOST_PYTHON_DYNAMIC_LIB',
+                                       'BOOST_ALL_NO_LIB'])
+
+        def BuildBoostPython(env, target, sources):
+            # Code to build "target" from "source"
+            targets = list(itertools.chain(*[env.SharedObject(None, source) for source in sources  if source.suffix in ['.cpp', '.cxx', '.c++']]))
+            sources = [source for source in sources if header.suffix == '.h']
+            SYSTEM = env['SYSTEM']
+            if SYSTEM == 'linux' and len(sources) == 1:
+                cmd = env.Command(sources[0].target_from_source('', '.h.gch'), header, '$CXX -o $TARGET -x c++-header -c -fPIC $SHCXXFLAGS $_CCCOMCOM $SOURCE')
+                env.Depends(targets, cmd)
+
+            source = env.File('response_file.rsp')
+            with open(source.abspath, 'w') as filehandler:
+                filehandler.write(' '.join(target.abspath.replace('\\','/') + ' ' for target in targets))
+            env.Append(LINKFLAGS = '@' + source.abspath)
+
+            kwargs = dict(SHLIBSUFFIX = '.so',
+                          SHLIBPREFIX = '')
+            if SYSTEM == 'osx':
+                bpm = env.LoadableModule(target, [], LDMODULESUFFIX='.so',
+                    FRAMEWORKSFLAGS = '-flat_namespace -undefined suppress', **kwargs)
+            else:
+                bpm = env.LoadableModule(target, [], **kwargs)
+
+            return bpm
+
+        env.BuildBoostPython = MethodType(BuildBoostPython, env)
+        env.Tool('python')
+
+def exists(env):
+    return 1
---
+++ conda/python-scons/toolchains.py
@@ -0,0 +1,31 @@
+from distutils.version import StrictVersion
+
+def generate(env):
+    """Add Builders and construction variables to the Environment."""
+    if not 'toolchains' in env['TOOLS'][:-1]:
+      env.Tool('default')
+      env.Tool('system')
+      SYSTEM = env['SYSTEM']
+      if SYSTEM == 'win':
+        if StrictVersion('8.0') <= StrictVersion(env['MSVC_VERSION']) < StrictVersion('10.0'):
+            env['LINKCOM'].append('mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;1')
+            env['SHLINKCOM'].append('mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;2')
+        env.AppendUnique(CCFLAGS=['/O2',
+                                  '/Ob2',
+                                  '/MD',
+                                  '/GR',
+                                  '/EHsc',
+                                  '/Gy',
+                                  '/GF',
+                                  '/GA'],
+                         CPPDEFINES=['WIN32',
+                                     'UNICODE'])
+        env.PrependUnique(CPPPATH=['$PREFIX\include'])
+        env.PrependUnique(LIBPATH=['$PREFIX\lib',
+                                   '$PREFIX\..\libs'])
+      else:
+        env.PrependUnique(CPPPATH=['$PREFIX/include'],
+                          LIBPATH=['$PREFIX/lib'])
+
+def exists(env):
+    return 1
