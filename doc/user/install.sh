5edb6ce220ce4d2d5344ed1efca30548226d0181
Pierre Fernique <pfernique@gmail.com>
2017-01-20 15:04:18 +0100

    Update documentation

---
+++ doc/user/install.sh
@@ -0,0 +1,45 @@
+set -ev
+
+if [[ "$CONDA_DIR" = "" ]]; then
+    CONDA_DIR='$HOME/miniconda'
+fi
+cd $CONDA_PREFIX
+if [[ ! "$CONDA_VERSION" = "" ]]; then
+    CONDA_VERSION=2
+fi
+
+if [[ ! -d CONDA_DIR ]]; then
+
+    case "$(uname -s)" in
+        Darwin)
+            curl https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-MacOSX-x86_64.sh -o miniconda.sh
+            ;;
+        Linux)
+            wget https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh -O miniconda.sh
+            ;;
+        *)
+            exit 1 
+            ;;
+    esac
+
+    bash miniconda.sh -b -p $CONDA_DIR
+    rm miniconda.sh
+fi
+
+export PATH=$CONDA_DIR/bin:$PATH
+
+if [[ "$CONDA_ALWAYS_YES" = "" ]]; then
+  CONDA_ALWAYS_YES=no
+fi
+if [[ "$CONDA_CHANGE_PS1" = "" ]]; then
+  CONDA_CHANGE_PS1=yes
+fi
+conda config --set always_yes $CONDA_ALWAYS_YES
+conda config --set changeps1 $CONDA_CHANGE_PS1
+conda update -q conda
+
+# if [[ -d $HOME/.config/sublime-text-3/Packages/SublimeREPL ]]; then
+#     mkdir -p $HOME/.config/sublime-text-3/Packages/StatisKit
+# fi
+
+set +ev
