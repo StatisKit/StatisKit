1df4c5998deac2f136536a9e74377d5363a9234f
Pierre Fernique <pfernique@gmail.com>
2017-01-20 12:10:13 +0100

    Update toolchain.py

--- doc/user/install.sh
+++ doc/user/install.sh
@@ -1,17 +1,32 @@
-set -e
+set -ev
 
-if [[ ! "$CONDA_PREFIX" = "" ]]; then
-    cd $CONDA_PREFIX
-else
-    CONDA_PREFIX=`pwd`
+if [[ "$CONDA_PREFIX" = "" ]]; then
+    CONDA_PREFIX='$HOME/.conda'
 fi
+cd $CONDA_PREFIX
 if [[ ! "$CONDA_VERSION" = "" ]]; then
     CONDA_VERSION=2
 fi
-wget https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $CONDA_PREFIX/miniconda$CONDA_VERSION
-rm miniconda.sh
-export PATH=$CONDA_PREFIX/miniconda/bin:$PATH
+
+if [[ ! -d CONDA_PREFIX ]]; then
+
+    case "$(uname -s)" in
+        Darwin)
+            curl https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-MacOSX-x86_64.sh -o miniconda.sh
+            ;;
+        Linux)
+            wget https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh -O miniconda.sh
+            ;;
+        *)
+            exit 1 
+            ;;
+    esac
+
+    bash miniconda.sh -b -p $CONDA_PREFIX
+    rm miniconda.sh
+fi
+
+export PATH=$CONDA_PREFIX/bin:$PATH
 
 if [[ "$CONDA_ALWAYS_YES" = "" ]]; then
   CONDA_ALWAYS_YES=no
@@ -21,9 +36,10 @@ if [[ "$CONDA_CHANGE_PS1" = "" ]]; then
 fi
 conda config --set always_yes $CONDA_ALWAYS_YES
 conda config --set changeps1 $CONDA_CHANGE_PS1
-#if [[ ! "$CONDA_BUILD_ROOT_DIR" = "" ]]; then
-#  echo "conda-build:" >> $HOME/.condarc;
-#  echo "  root-dir: " $CONDA_BUILD_ROOT_DIR >> $HOME/.condarc;
-#fi
 conda update -q conda
-rm install.sh
+
+# if [[ -d $HOME/.config/sublime-text-3/Packages/SublimeREPL ]]; then
+#     mkdir -p $HOME/.config/sublime-text-3/Packages/StatisKit
+# fi
+
+set +ev
