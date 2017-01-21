f0bd5237bc992fe990db9d5cc0b874f444fc5cf4
Pierre Fernique <pfernique@gmail.com>
2017-01-21 14:04:48 +0100

    Update install.bat

--- doc/user/install.bat
+++ doc/user/install.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 echo ON
 
 if "%CONDA_VERSION%"=="" set CONDA_VERSION=2
-if "%CONDA_DIR%"=="" set CONDA_DIR='%USERPROFILE%\Miniconda!CONDA_VERSION!'
+if "%CONDA_DIR%"=="" set CONDA_DIR=%USERPROFILE%\Miniconda!CONDA_VERSION!
 
 if "%PLATFORM%"=="" set PLATFORM=%PROCESSOR_ARCHITECTURE%
 if not "!PLATFORM!"=="x86" set PLATFORM=x86_64
@@ -14,26 +14,26 @@ if !ERRORLEVEL! neq 0 (
     exit /b !ERRORLEVEL!
 )
 
-if not exist %CONDA_DIR% (
-    curl https://repo.continuum.io/miniconda/Miniconda%CONDA_VERSION%-latest-Windows-%PLATFORM%.exe -o miniconda.exe
+if not exist!CONDA_DIR! (
+    curl https://repo.continuum.io/miniconda/Miniconda!CONDA_VERSION!-latest-Windows-!PLATFORM!.exe -o miniconda.exe
     if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
-    start /wait "" miniconda.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%CONDA_DIR%
+    start /wait "" miniconda.exe /InstallationType=JustMe /RegisterPython=0 /S /D=!CONDA_DIR!
     if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
     del miniconda.exe
     if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
 )
 
-set PATH=%CONDA_DIR%;%CONDA_DIR%\Scripts;%PATH%
+attrib +h !CONDA_DIR!
+if !ERRORLEVEL! neq 0 echo Failed to hide the !CONDA_DIR! directory
 
-if [[ "$CONDA_ALWAYS_YES" = "" ]]; then
-  CONDA_ALWAYS_YES=no
-fi
-if [[ "$CONDA_CHANGE_PS1" = "" ]]; then
-  CONDA_CHANGE_PS1=yes
-fi
-conda config --set always_yes $CONDA_ALWAYS_YES
+set PATH=!CONDA_DIR!;!CONDA_DIR!\Scripts;%PATH%
+
+if "%CONDA_ALWAYS_YES%"=="" set CONDA_ALWAYS_YES=no
+if "%CONDA_CHANGE_PS1%"=="" set CONDA_CHANGE_PS1=yes
+
+conda config --set always_yes !CONDA_ALWAYS_YES!
 if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
-conda config --set changeps1 $CONDA_CHANGE_PS1
+conda config --set changeps1 !CONDA_CHANGE_PS1!
 if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
 conda update -q conda
 if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
