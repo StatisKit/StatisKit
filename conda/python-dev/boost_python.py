c8168ece7b730ef5ce67a400345246c20bddbc89
Pierre Fernique <pfernique@gmail.com>
2017-01-23 11:34:35 +0100

    Update boost_python.py

--- conda/python-dev/boost_python.py
+++ conda/python-dev/boost_python.py
@@ -30,7 +30,7 @@ def generate(env):
                                                                                                + ' $SOURCE')
                 env.Depends(targets, cmd)
                 if SYSTEM == 'osx':
-                    env['CXX'] += " -include " + sources[0].target_from_source('', '.h').for_signature()
+                    env['CXX'] += " -include " + str(sources[0].target_from_source('', '.h'))
             env.Depends(target, targets)
             response = env.Textfile('response_file.rsp',
                          [tgt.abspath.replace('\\','/') for tgt in targets],
