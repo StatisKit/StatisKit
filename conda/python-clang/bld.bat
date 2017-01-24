837f1445350bf7113f439d67b46cd866a5e80c90
Pierre Fernique <pfernique@gmail.com>
2017-01-24 14:24:34 +0100

    Update python-clang

--- conda/python-clang/bld.bat
+++ conda/python-clang/bld.bat
@@ -1,7 +1,7 @@
 mkdir %SP_DIR%\clang
 xcopy bindings\python\clang %SP_DIR%\clang /sy
 echo "from path import path" >> %SP_DIR%\clang\cindex.py
-echo "library_path = path(__path__)" >> %SP_DIR%\clang\cindex.py
+echo "library_path = path(__file__)" >> %SP_DIR%\clang\cindex.py
 echo "while not (library_path/'Library').exists()\:" >> %SP_DIR%\clang\cindex.py
 echo "    library_path = library_path.parent" >> %SP_DIR%\clang\cindex.py
 echo "library_path = library_path/'Library'" >> %SP_DIR%\clang\cindex.py
