5694b21b6ac3bcfce2d629e4b3afe3a6bbd4760b
Pierre Fernique <pfernique@gmail.com>
2017-01-25 08:42:43 +0100

    Update build.sh

--- conda/python-clang/build.sh
+++ conda/python-clang/build.sh
@@ -4,4 +4,4 @@ echo "from path import path" >> $SP_DIR/clang/cindex.py
 echo "library_path = path(__file__)" >> $SP_DIR/clang/cindex.py
 echo "while not (library_path/'lib').exists():" >> $SP_DIR/clang/cindex.py
 echo "    library_path = library_path.parent" >> $SP_DIR/clang/cindex.py
-echo "conf.set_library_path((library_path/'lib').asbpath())" >> $SP_DIR/clang/cindex.py
+echo "conf.set_library_path((library_path/'lib').abspath())" >> $SP_DIR/clang/cindex.py
