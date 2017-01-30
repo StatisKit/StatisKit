f3e10ad0280fdf3c6a16cbfcb358e4ec2cb4f468
Pierre Fernique <pfernique@gmail.com>
2017-01-30 12:13:57 +0100

    Update boost_python.py

--- conda/python-dev/boost_python.py
+++ conda/python-dev/boost_python.py
@@ -15,6 +15,8 @@ def generate(env):
             SYSTEM = env['SYSTEM']
             if not SYSTEM == 'win':
                 target += '.so'
+            else:
+                target += '.pyd'
             target = env.File(target).srcnode()
             targets = list(itertools.chain(*[env.SharedObject(None, source) for source in sources  if source.suffix in ['.cpp', '.cxx', '.c++']]))
             sources = [source for source in sources if source.suffix == '.h']
