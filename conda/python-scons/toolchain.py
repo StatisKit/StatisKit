30ece181df62911686d3fa32d5f32c6470dbccb9
Pierre Fernique <pfernique@gmail.com>
2017-01-30 20:10:02 +0100

    Update toolchain.py

--- conda/python-scons/toolchain.py
+++ conda/python-scons/toolchain.py
@@ -32,9 +32,10 @@ def generate(env):
       env.Tool('default')
       env.Tool('prefix')
       if SYSTEM == 'win':
-        # if StrictVersion('8.0') <= StrictVersion(env['MSVC_VERSION']) < StrictVersion('10.0'):
-        #     env['LINKCOM'].append('mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;1')
-        #     env['SHLINKCOM'].append('mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;2')
+        if StrictVersion('8.0') <= StrictVersion(env['MSVC_VERSION']) < StrictVersion('10.0'):
+            #env['LINKCOM'].append('mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;1')
+            env['LINKCOM'] = [env['LINKCOM'], 'mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;1']
+            env['SHLINKCOM'] = [env['SHLINKCOM'], 'mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;2']
         env.AppendUnique(CCFLAGS=['/O2',
                                   '/Ob2',
                                   '/MD',
