5139629f96a7604c6538ca3335a59c639edd5890
Pierre Fernique <pfernique@gmail.com>
2017-02-01 09:51:38 +0100

    Update bld.bat

--- conda/vcs/bld.bat
+++ conda/vcs/bld.bat
@@ -8,11 +8,11 @@ git config --system alias.ci commit
 if errorlevel 1 exit 1
 git config --system alias.st status
 if errorlevel 1 exit 1
-git config --system alias.unstage reset HEAD --
+git config --system alias.unstage "reset HEAD --"
 if errorlevel 1 exit 1
-git config --system alias.last log -1 HEAD
+git config --system alias.last "log -1 HEAD"
 if errorlevel 1 exit 1
-git config --system credential.helper cache --timeout=3600
+git config --system credential.helper "cache --timeout=3600"
 if errorlevel 1 exit 1
 
 echo OFF
