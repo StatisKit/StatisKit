ff27788a7a2ee079927c9361fcdb5755a62ca117
Pierre Fernique <pfernique@gmail.com>
2017-02-01 12:48:14 +0100

    Update user_install.bat

--- doc/user/user_install.bat
+++ doc/user/user_install.bat
@@ -1,16 +1,14 @@
-setlocal EnableDelayedExpansion
-
 echo OFF
 
 if "%CONDA_VERSION%"=="" set CONDA_VERSION=2
-if "%CONDA_DIR%"=="" set CONDA_DIR=%USERPROFILE%\Miniconda!CONDA_VERSION!
+if "%CONDA_DIR%"=="" set CONDA_DIR=%USERPROFILE%\Miniconda%CONDA_VERSION%
 
 if "%PLATFORM%"=="" set PLATFORM=%PROCESSOR_ARCHITECTURE%
-if not "!PLATFORM!"=="x86" set PLATFORM=x86_64
+if not "%PLATFORM%"=="x86" set PLATFORM=x86_64
 
 where curl
 if errorlevel 1 (
-    if not exist Miniconda!CONDA_VERSION!-latest-Windows-!PLATFORM!.exe (
+    if not exist Miniconda%CONDA_VERSION%-latest-Windows-%PLATFORM%.exe (
         echo "You must first install the cURL program or download manually the Miniconda!CONDA_VERSION!-latest-Windows-!PLATFORM!.exe file"
         echo "User installation failed."
         exit 1
@@ -19,8 +17,8 @@ if errorlevel 1 (
 
 if "%BATCH_MODE%"=="" set BATCH_MODE=false
 
-if not exist !CONDA_DIR! (
-    if exist Miniconda!CONDA_VERSION!-latest-Windows-!PLATFORM!.exe (
+if not exist %CONDA_DIR% (
+    if exist Miniconda%CONDA_VERSION%-latest-Windows-%PLATFORM%.exe (
         set CLEAN_MINICONDA=0
     ) else (
         curl https://repo.continuum.io/miniconda/Miniconda!CONDA_VERSION!-latest-Windows-!PLATFORM!.exe -o miniconda.exe
@@ -44,11 +42,11 @@ if not exist !CONDA_DIR! (
     if "%CLEAN_MINICONDA%"=="1" del miniconda.exe
 )
 
-attrib +h !CONDA_DIR!
-if errorlevel 1 echo "Failed to hide the "!CONDA_DIR!" directory"
+attrib +h %CONDA_DIR%
+if errorlevel 1 echo "Failed to hide the "%CONDA_DIR%" directory"
 
-set PATH=!CONDA_DIR!;!CONDA_DIR!\Scripts;%PATH%
-call !CONDA_DIR!\Scripts\activate.bat root
+set PATH=%CONDA_DIR%;%CONDA_DIR%\Scripts;%PATH%
+call %CONDA_DIR%\Scripts\activate.bat root
 if errorlevel 1 (
     echo "Activation of Conda failed"
     echo "User installation failed."
@@ -58,13 +56,13 @@ if errorlevel 1 (
 if "%CONDA_ALWAYS_YES%"=="" set CONDA_ALWAYS_YES=no
 if "%CONDA_CHANGE_PS1%"=="" set CONDA_CHANGE_PS1=yes
 
-conda config --set always_yes !CONDA_ALWAYS_YES!
+conda config --set always_yes %CONDA_ALWAYS_YES%
 if errorlevel 1 (
     echo "Configuration of Conda failed"
     echo "User installation failed."
     exit 1
 )
-conda config --set changeps1 !CONDA_CHANGE_PS1!
+conda config --set changeps1 %CONDA_CHANGE_PS1%
 if errorlevel 1 (
     echo "Configuration of Conda failed"
     echo "User installation failed."
