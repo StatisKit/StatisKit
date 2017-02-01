d403bb7f1ed45fba40dccd01db3c544e4ce1100c
Pierre Fernique <pfernique@gmail.com>
2017-02-01 09:47:44 +0100

    Update developer_install.sh

--- doc/developer/developer_install.sh
+++ doc/developer/developer_install.sh
@@ -1,7 +1,7 @@
 set +v
 
 export CLEAN_INSTALL=1
-if [[ ! -f install.sh ]]; then
+if [[ ! -f user_install.sh ]]; then
     wget http://statiskit.readthedocs.io/en/latest/_downloads/user_install.sh -O user_install.sh
     if [[ ! "$?" = "0" ]]; then
         echo "Download of the user_install.sh file for Conda installation failed"
