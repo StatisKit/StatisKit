e61bb238a2e8af5e2797b0bfa08353bce8f25174
Pierre Fernique <pfernique@gmail.com>
2017-02-04 17:46:37 +0100

    Update Conda Tool [skip ci]

--- conda/python-scons/conda.py
+++ conda/python-scons/conda.py
@@ -12,14 +12,22 @@ def generate(env):
     if not 'conda' in env['TOOLS'][:-1]:
       env.Tool('system')
 
-      AddOption('--anaconda-channels',
-                dest = 'anaconda-channels',
+      AddOption('--conda-channels',
+                dest = 'conda-channels',
                 type = 'string',
                 nargs = '+',
                 action = 'store',
                 help = 'Channels to use for Conda build and install',
                 default = ['statiskit', 'conda-forge'])
-      env['ANACONDA_CHANNELS'] = list(itertools.chain(*[['-c', channel] for channel in GetOption('anaconda-channels')]))
+      env['CONDA_CHANNELS'] = list(itertools.chain(*[['-c', channel] for channel in GetOption('conda-channels')]))
+
+      AddOption('--anaconda-channel',
+                dest = 'anaconda-channel',
+                type = 'string',
+                action = 'store',
+                help = 'Anaconda channel to use for upload',
+                default = '')
+      env['ANACONDA_CHANNEL'] = GetOption('anaconda-channel')
 
     def list_recipes(env, sources):
         SYSTEM = env['SYSTEM']
@@ -47,11 +55,11 @@ def generate(env):
         return [graph.node[recipe]['path'] for recipe in networkx.topological_sort(graph)]
 
     def CondaPackages(env, sources):
-        ANACONDA_CHANNELS =  env['ANACONDA_CHANNELS']
+        CONDA_CHANNELS =  env['CONDA_CHANNELS']
         conda, recipes = list_recipes(env, sources)
         targets = []
         for recipe in order_recipes('build', recipes):
-            cmd = [conda, 'build', str(recipe)] + ANACONDA_CHANNELS
+            cmd = [conda, 'build', str(recipe)] + CONDA_CHANNELS
             try:
                 targets.append(env.Command(subprocess.check_output(cmd + ['--output']).strip(), recipe, " ".join(cmd)))
             except:
@@ -61,7 +69,7 @@ def generate(env):
     env.AddMethod(CondaPackages)
 
     def CondaEnvironment(env, sources):
-        ANACONDA_CHANNELS = env['ANACONDA_CHANNELS']
+        CONDA_CHANNELS = env['CONDA_CHANNELS']
         conda, recipes = list_recipes(env, sources)
         CONDA_ENVIRONMENT = path(conda).parent.parent
         targets = []
@@ -70,7 +78,7 @@ def generate(env):
             target = os.path.join(CONDA_ENVIRONMENT,
                                   'conda-meta',
                                   archive.name.replace('.tar.bz2', '.json', 1))
-            target = env.Command(target, recipe, conda + " install -n " + CONDA_ENVIRONMENT.name + " " + recipe.name + " -y --use-local " + " ".join(ANACONDA_CHANNELS))
+            target = env.Command(target, recipe, conda + " install -n " + CONDA_ENVIRONMENT.name + " " + recipe.name + " -y --use-local " + " ".join(CONDA_CHANNELS))
             env.Depends(target, archive)
             targets.extend(target)
             target = path(targets[-1].abspath)
@@ -81,6 +89,23 @@ def generate(env):
         return targets
 
     env.AddMethod(CondaEnvironment)
+
+    def AnacondaUpload(env, sources):
+        ANACONDA_CHANNEL = env['ANACONDA_CHANNEL']
+        SYSTEM = env['SYSTEM']
+        conda, recipes = list_recipes(env, sources)
+        if SYSTEM == 'win':
+            anaconda = subprocess.check_output(['where', 'anaconda']).strip()
+        else:
+            anaconda = subprocess.check_output(['which', 'anaconda']).strip()
+        CONDA_ENVIRONMENT = path(conda).parent.parent
+        targets = []
+        for recipe in order_recipes('run', recipes):
+            archive = path(subprocess.check_output([conda, 'build', str(recipe), '--output']).strip())
+            targets.extend(env.Command('', archive, anaconda + " upload " + archive.abspath() + " -u " * ANACONDA_CHANNEL + ANACONDA_CHANNEL))
+        return targets
+
+    env.AddMethod(AnacondaUpload)
 
 def exists(env):
     return 1
