9b617a06d5c0c44a30e431ef054a771a6dbb30a4
Pierre Fernique <pfernique@gmail.com>
2017-02-04 11:08:22 +0100

    Update Conda tool [skip ci]

--- conda/python-scons/conda.py
+++ conda/python-scons/conda.py
@@ -9,6 +9,7 @@ def generate(env):
     """Add Builders and construction variables to the Environment."""
     if not 'conda' in env['TOOLS'][:-1]:
       env.Tool('system')
+
       AddOption('--anaconda-channels',
                 dest = 'anaconda-channels',
                 type = 'string',
@@ -16,54 +17,60 @@ def generate(env):
                 action = 'store',
                 help = 'Channels to use for Conda build and install',
                 default = ['statiskit', 'conda-forge'])
-      env['ANACONDA_CHANNELS'] = GetOption('anaconda-channels')
-      def BuildConda(env, target, sources=[], exclude=[]):
-        ANACONDA_CHANNELS = env.get('ANACONDA_CHANNELS')
-        # ANACONDA_USERNAME = env.get('ANACONDA_USERNAME')
-        channels = list(itertools.chain(*[['-c', channel] for channel in ANACONDA_CHANNELS]))
-        targets = []
+      env['ANACONDA_CHANNELS'] = list(itertools.chain(*[['-c', channel] for channel in GetOption('anaconda-channels')]))
+
+    def list_recipes(env, sources, exclude):
         SYSTEM = env['SYSTEM']
         if len(sources) == 0:
             sources = [env.Dir(".").srcnode()]
         if SYSTEM == 'win':
             conda = subprocess.check_output(['where', 'conda']).strip()
-            recipes = [recipe for source in sources for recipe in path(source.abspath).walkdirs() if (recipe/'meta.yaml').exists() and (recipe/'bld.bat').exists()]
+            recipes = [recipe for source in sources for recipe in path(source.abspath).dirs() if (recipe/'meta.yaml').exists() and (recipe/'bld.bat').exists() and recipe not in exclude]
         else:
             conda = subprocess.check_output(['which', 'conda']).strip()
-            recipes = [recipe for source in sources for recipe in path(source.abspath).walkdirs() if (recipe/'meta.yaml').exists() and (recipe/'build.sh').exists()]
-        targets = []
-        def compute_recipes(mode):
-            graph = networkx.DiGraph()
-            for recipe in recipes:
-                graph.add_node(str(recipe.name), path=recipe.abspath())
-            for recipe in recipes:
-                with open(recipe/'meta.yaml', 'r') as filehandler:
-                    rendered = ''.join(filehandler.readlines()).replace('{{ ', '$').replace(' }}', '')
-                    requirements = yaml.load(rendered).get('requirements', {}).get(mode, {})
-                for requirement in requirements:
-                    requirement = requirement.split()[0]
-                    if requirement in graph:
-                        graph.add_edge(requirement, str(recipe.name))
-            return [graph.node[recipe]['path'] for recipe in networkx.topological_sort(graph)]
+            recipes = [recipe for source in sources for recipe in path(source.abspath).dirs() if (recipe/'meta.yaml').exists() and (recipe/'build.sh').exists() and recipe not in exclude]
+        return conda, recipes
 
-        for recipe in compute_recipes('build'):
-            cmd = [conda, 'build', str(recipe)] + channels
+    def order_recipes(mode, recipes):
+        graph = networkx.DiGraph()
+        for recipe in recipes:
+            graph.add_node(str(recipe.name), path=recipe.abspath())
+        for recipe in recipes:
+            with open(recipe/'meta.yaml', 'r') as filehandler:
+                rendered = ''.join(filehandler.readlines()).replace('{{ ', '$').replace(' }}', '')
+                requirements = yaml.load(rendered).get('requirements', {}).get(mode, {})
+            for requirement in requirements:
+                requirement = requirement.split()[0]
+                if requirement in graph:
+                    graph.add_edge(requirement, str(recipe.name))
+        return [graph.node[recipe]['path'] for recipe in networkx.topological_sort(graph)]
+
+
+      def BuildConda(env, sources=[], exclude=[]):
+        ANACONDA_CHANNELS =  env['ANACONDA_CHANNELS']
+        conda, recipes = list_recipes(env, sources, exclude)
+        targets = []
+        for recipe in order_recipes('build', recipes):
+            cmd = [conda, 'build', str(recipe)] + ANACONDA_CHANNELS
             try:
                 targets.append(env.Command(subprocess.check_output(cmd + ['--output']).strip(), recipe, " ".join(cmd)))
             except:
                 pass
-        if target == 'install':
-            CONDA_ENVIRONMENT = path(conda).parent.parent
-            target = env.Command(CONDA_ENVIRONMENT, compute_recipes('run'), conda + " install $SOURCES " + " ".join(channels))
-            env.NoClean(target)
-            for recipe in targets:
-                env.Depends(target, recipe)
-            return target
-        else:
-            return targets
         return targets
 
       env.AddMethod(BuildConda)
+
+      def InstallConda(env, sources=[], exclude=[]):
+        ANACONDA_CHANNELS = env['ANACONDA_CHANNELS']
+        conda, recipes = list_recipes(env, sources, exclude)
+        CONDA_ENVIRONMENT = path(conda).parent.parent
+        target = env.Command(CONDA_ENVIRONMENT, [recipe.name for recipe in order_recipes('run', recipes)], conda + " install $SOURCES " + " ".join(ANACONDA_CHANNELS))
+        env.NoClean(target)
+        for recipe in env.BuildConda(sources, exclude):
+            env.Depends(target, recipe)
+        return target
+
+      env.AddMethod(InstallConda)
 
 def exists(env):
     return 1
