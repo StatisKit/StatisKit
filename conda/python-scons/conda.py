b2a44f30901acb1cb64be32a8959fe23fbe8348b
Pierre Fernique <pfernique@gmail.com>
2017-02-04 14:30:17 +0100

    Update Conda Tool [skip ci]

--- conda/python-scons/conda.py
+++ conda/python-scons/conda.py
@@ -4,6 +4,8 @@ import itertools
 import yaml
 import networkx
 from SCons.Script import AddOption, GetOption
+import json
+import os
 
 def generate(env):
     """Add Builders and construction variables to the Environment."""
@@ -21,15 +23,18 @@ def generate(env):
 
     def list_recipes(env, sources, exclude):
         SYSTEM = env['SYSTEM']
+        src_dir = path(env.Dir(".").srcnode().abspath)
         if len(sources) == 0:
-            sources = [env.Dir(".").srcnode()]
+            sources = [src_dir]
+        else:
+            sources = [path(source).abspath() for source in sources]
         if SYSTEM == 'win':
             conda = subprocess.check_output(['where', 'conda']).strip()
-            recipes = [recipe for source in sources for recipe in path(source.abspath).dirs() if (recipe/'meta.yaml').exists() and (recipe/'bld.bat').exists() and recipe not in exclude]
+            recipes = [recipe for source in sources for recipe in source.dirs() if (recipe/'meta.yaml').exists() and (recipe/'bld.bat').exists()]
         else:
             conda = subprocess.check_output(['which', 'conda']).strip()
-            recipes = [recipe for source in sources for recipe in path(source.abspath).dirs() if (recipe/'meta.yaml').exists() and (recipe/'build.sh').exists() and recipe not in exclude]
-        return conda, recipes
+            recipes = [recipe for source in sources for recipe in source.dirs() if (recipe/'meta.yaml').exists() and (recipe/'build.sh').exists()]
+        return conda, [recipe for recipe in recipes if recipe.relpath(src_dir) not in exclude]
 
     def order_recipes(mode, recipes):
         graph = networkx.DiGraph()
@@ -45,8 +50,7 @@ def generate(env):
                     graph.add_edge(requirement, str(recipe.name))
         return [graph.node[recipe]['path'] for recipe in networkx.topological_sort(graph)]
 
-
-      def BuildConda(env, sources=[], exclude=[]):
+    def BuildConda(env, sources=[], exclude=[]):
         ANACONDA_CHANNELS =  env['ANACONDA_CHANNELS']
         conda, recipes = list_recipes(env, sources, exclude)
         targets = []
@@ -58,19 +62,31 @@ def generate(env):
                 pass
         return targets
 
-      env.AddMethod(BuildConda)
+    env.AddMethod(BuildConda)
 
-      def InstallConda(env, sources=[], exclude=[]):
+    def InstallConda(env, sources=[], exclude=[]):
         ANACONDA_CHANNELS = env['ANACONDA_CHANNELS']
         conda, recipes = list_recipes(env, sources, exclude)
         CONDA_ENVIRONMENT = path(conda).parent.parent
-        target = env.Command(CONDA_ENVIRONMENT, [recipe.name for recipe in order_recipes('run', recipes)], conda + " install $SOURCES " + " ".join(ANACONDA_CHANNELS))
-        env.NoClean(target)
-        for recipe in env.BuildConda(sources, exclude):
-            env.Depends(target, recipe)
+        targets = []
+        import pdb
+        pdb.set_trace()
+        for recipe in order_recipes('run', recipes):
+            archive = path(subprocess.check_output([conda, 'build', str(recipe), '--output']).strip())
+            target = os.path.join(CONDA_ENVIRONMENT,
+                                  'conda-meta',
+                                  archive.name.replace('.tar.bz2', '.json', 1))
+            target = env.Command(target, [recipe, archive], conda + " install -n " + CONDA_ENVIRONMENT.name + " " + recipe.name + " -y --use-local " + " ".join(ANACONDA_CHANNELS))
+            env.Depends(target, archive)
+            targets.extend(target)
+            target = path(targets[-1].abspath)
+            if target.exists():
+                with open(target, 'r') as filehandler:
+                    for filename in json.loads("".join(filehandler.readlines())).get('files', []):
+                        env.Clean(target, os.path.join(CONDA_ENVIRONMENT, filename))
         return target
 
-      env.AddMethod(InstallConda)
+    env.AddMethod(InstallConda)
 
 def exists(env):
     return 1
