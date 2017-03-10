e7428db13214bdf82d7395966cfbbd2bf8d24f43
Pierre Fernique <pfernique@gmail.com>
2017-03-10 10:05:10 +0100

    Update Toolchain for MSVC 12

--- conda/python-scons/toolchain.py
+++ conda/python-scons/toolchain.py
@@ -39,7 +39,7 @@ def generate(env):
               default = 'always',
               choices=['always', 'never'])
         env['DIAGNOSTICS_COLOR'] = GetOption('diagnostics-color')
-    print env['ARCH'], env['TARGET_ARCH'], env['MSVC_VERSION']
+      print env['ARCH'], env['TARGET_ARCH'], env['MSVC_VERSION']
       env.Tool('default')
       env.Tool('prefix')
       if SYSTEM == 'win':
