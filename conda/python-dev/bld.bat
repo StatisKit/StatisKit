bed27a788a6297cbef917ebd14f5ebe8672f6021
Pierre Fernique <pfernique@gmail.com>
2017-03-17 15:31:24 +0100

    Add ipdb debugger within nose

--- conda/python-dev/bld.bat
+++ conda/python-dev/bld.bat
@@ -1,5 +1,6 @@
 echo ON
 
+pip install ipdbplugin
 copy %RECIPE_DIR%\boost_python.py %SP_DIR%\SCons\site_scons\site_tools\boost_python.py
 if errorlevel 1 exit 1
 copy %RECIPE_DIR%\python.py %SP_DIR%\SCons\site_scons\site_tools\python.py
