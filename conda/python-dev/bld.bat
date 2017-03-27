78d48f52eac2f5bc2fd54ab617762b1e9b8b6a6f
Pierre Fernique <pfernique@gmail.com>
2017-03-27 22:31:38 +0200

    Update recipe for python-dev

--- conda/python-dev/bld.bat
+++ conda/python-dev/bld.bat
@@ -1,5 +1,6 @@
 echo ON
 
+pip install ipdb
 pip install ipdbplugin
 copy %RECIPE_DIR%\boost_python.py %SP_DIR%\SCons\site_scons\site_tools\boost_python.py
 if errorlevel 1 exit 1
--- conda/python-dev/build.sh
+++ conda/python-dev/build.sh
@@ -1,5 +1,6 @@
 set -ve
 
+pip install ipdb
 pip install ipdbplugin
 cp $RECIPE_DIR/boost_python.py $SP_DIR/SCons/site_scons/site_tools/boost_python.py
 cp $RECIPE_DIR/python.py $SP_DIR/SCons/site_scons/site_tools/python.py
