9ab6b0036bc35cd40804000219abece99eb10458
Pierre Fernique <pfernique@gmail.com>
2017-07-25 13:33:29 +0200

    Update boost_python.py

--- conda/python-dev/boost_python.py
+++ conda/python-dev/boost_python.py
@@ -62,6 +62,7 @@ def generate(env):
                                           SHLINKFLAGS='$LINKFLAGS -bundle',
                                           FRAMEWORKSFLAGS='-flat_namespace -undefined suppress')
             else:
+                nv.AppendUnique(CCFLAGS=['-Wattributes', '-Wdeprecated-declarations'])
                 return env.LoadableModule(target, [], SHLIBPREFIX='')
 
         env.AddMethod(BoostPythonExtension)
