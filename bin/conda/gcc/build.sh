602dabc75a6d4afddbf927cf0f341322bfc485a3
Pierre Fernique <pfernique@gmail.com>
2017-09-22 14:19:59 +0200

    Update CI

--- bin/conda/gcc/build.sh
+++ bin/conda/gcc/build.sh
@@ -4,7 +4,7 @@ echo 'gcc version'
 gcc -v
 
 #export TMPDIR=/media/osboxes/New/tmpdir
-export TMPDIR=/media/NewPartition/tmpdir
+export TMPDIR=$PWD/tmpdir
 
 #./contrib/download_prerequisites
 
@@ -17,18 +17,18 @@ if [ "$(uname)" == "Darwin" ]; then
     export LDFLAGS="-Wl,-headerpad_max_install_names"
     export BOOT_LDFLAGS="-Wl,-headerpad_max_install_names"
 
-     $PWD/../gcc-4.8.2-20140120/configure \
+     $PWD/../gcc-7.1.1-20170718/configure \
         --prefix=$PREFIX \
         --disable-multilib \
         --enable-languages=c,c++ 
-        #—libdir=$PREFIX/lib \
-        #-—with-gmp=$PREFIX \
-        #—-with-mpfr=$PREFIX \
-        #—-with-mpc=$PREFIX \
-        #-—with-isl=$PREFIX \
-        #--with-cloog=$PREFIX \
-        #--with-boot-ldflags=$LDFLAGS \
-        #--with-stage1-ldflags=$LDFLAGS \
+        --libdir=$PREFIX/lib \
+        --with-gmp=$PREFIX \
+        --with-mpfr=$PREFIX \
+        --with-mpc=$PREFIX \
+        --with-isl=$PREFIX \
+        --with-cloog=$PREFIX \
+        --with-boot-ldflags=$LDFLAGS \
+        --with-stage1-ldflags=$LDFLAGS \
 
 else
     # For reference during post-link.sh, record some
@@ -36,7 +36,10 @@ else
     #mkdir -p ${PREFIX}/share
     #cat /etc/*-release > ${PREFIX}/share/conda-gcc-build-machine-os-details
     
-      $PWD/../gcc-4.8.2-20140120/configure \
+      $PWD/../gcc-7.1.1-20170718/configure \
+        --build=x86_64-linux-gnu \
+        --host=x86_64-linux-gnu \
+        --target=x86_64-linux-gnu \
         --prefix=$PREFIX \
         --libdir=$PREFIX/lib \
         --with-gmp=$PREFIX \
