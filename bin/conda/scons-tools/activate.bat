6c30656ef90e9a5dfcd166cd9cb396a8eca6aed3
Pierre Fernique <pfernique@gmail.com>
2017-10-02 17:29:40 +0200

    Update

--- bin/conda/scons-tools/activate.bat
+++ bin/conda/scons-tools/activate.bat
@@ -1,4 +1,8 @@
+echo ON
+
 python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" > temp.txt
 set /p STK_SP_DIR=<temp.txt
 del temp.txt
 set SCONSFLAGS=%SCONSFLAGS% --site-dir=%STK_SP_DIR%\scons-tools
+
+echo OFF
