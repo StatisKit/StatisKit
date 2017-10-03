ec84531fa46ca15a55dd1012a7912f89c9b18c70
Pierre Fernique <pfernique@gmail.com>
2017-10-03 09:10:12 +0200

    Update

--- bin/conda/scons-tools/activate.bat
+++ bin/conda/scons-tools/activate.bat
@@ -1,7 +1,7 @@
 python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" > temp.txt
 set /p SITE_SCONS=<temp.txt
 del temp.txt
-sep SITE_SCONS=%SITE_SCONS%\scons_tools
+set SITE_SCONS=%SITE_SCONS%\scons_tools
 if "%SCONSFLAGS%"=="" (
     set SCONSFLAGS=--site-dir=%SITE_SCONS%
 ) else (
