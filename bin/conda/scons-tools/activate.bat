7f8d50700f3ee9d2ed2ab38579a9edc7ba2dc0e8
Pierre Fernique <pfernique@gmail.com>
2017-10-03 07:17:38 +0200

    Updqte

--- bin/conda/scons-tools/activate.bat
+++ bin/conda/scons-tools/activate.bat
@@ -3,7 +3,11 @@ echo ON
 python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" > temp.txt
 set /p TMP_DIR=<temp.txt
 del temp.txt
-set SCONSFLAGS=%SCONSFLAGS% --site-dir=%TMP_DIR%\scons_tools
+if %SCONSFLAGS%=="" (
+    set SCONSFLAGS=--site-dir=%TMP_DIR%\scons_tools
+) else (
+    set SCONSFLAGS=%SCONSFLAGS% --site-dir=%TMP_DIR%\scons_tools
+)
 set TMP_DIR=
 
 echo OFF
--- bin/conda/scons-tools/activate.sh
+++ bin/conda/scons-tools/activate.sh
@@ -1,7 +1,11 @@
 set -ve
 
 export TMP_DIR=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
-export SCONSFLAGS=$SCONSFLAGS --site-dir=$TMP_DIR/scons_tools
+if [[ "$SCONSFLAGS" = "" ]]; then
+    export SCONSFLAGS=$SCONSFLAGS --site-dir=$TMP_DIR/scons_tools
+else
+    export SCONSFLAGS=--site-dir=$TMP_DIR/scons_tools
+fi
 unset TMP_DIR
 
 set +ve
