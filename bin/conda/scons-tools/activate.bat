7d4f02d68cb8b6118aef605744a3511e7c3bd466
Pierre Fernique <pfernique@gmail.com>
2017-10-03 06:55:56 +0200

    Update

--- bin/conda/scons-tools/activate.bat
+++ bin/conda/scons-tools/activate.bat
@@ -3,7 +3,7 @@ echo ON
 python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" > temp.txt
 set /p TMP_DIR=<temp.txt
 del temp.txt
-set SCONSFLAGS=%SCONSFLAGS% --site-dir=%TMP_DIR%\scons-tools
+set SCONSFLAGS=%SCONSFLAGS% --site-dir=%TMP_DIR%\scons_tools
 set TMP_DIR=
 
 echo OFF
--- bin/conda/scons-tools/activate.sh
+++ bin/conda/scons-tools/activate.sh
@@ -1,3 +1,7 @@
+set -ve
+
 export TMP_DIR=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 export SCONSFLAGS=$SCONSFLAGS --site-dir=$TMP_DIR/scons_tools
 unset TMP_DIR
+
+set +ve
