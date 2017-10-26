bc31f85d5ff9ef13e83a088e06a30ef03dbb1ed9
Pierre Fernique <pfernique@gmail.com>
2017-10-26 11:05:41 +0200

    Update scons-tools

--- bin/conda/scons-tools/activate.sh
+++ bin/conda/scons-tools/activate.sh
@@ -1,7 +1,7 @@
 export SITE_SCONS=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 export SITE_SCONS=$SITE_SCONS/scons_tools
 if [[ "$SCONSFLAGS" = "" ]]; then
-    export SCONSFLAGS=--site-dir=$SITE_SCONS
+    export SCONSFLAGS="--site-dir=$SITE_SCONS"
 else
-    export SCONSFLAGS=$SCONSFLAGS --site-dir=$SITE_SCONS
+    export SCONSFLAGS=$SCONSFLAGS" --site-dir="$SITE_SCONS
 fi
