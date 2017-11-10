086369b8d2cd8906b0828d676b494e556b54c485
Pierre Fernique <pfernique@gmail.com>
2017-11-10 09:06:39 +0100

    Update vcs

--- bin/conda/vcs/build.sh
+++ bin/conda/vcs/build.sh
@@ -6,6 +6,5 @@ git config --system alias.ci commit
 git config --system alias.st status
 git config --system alias.unstage 'reset HEAD --'
 git config --system alias.last 'log -1 HEAD'
-git config --system credential.helper 'cache --timeout=3600'
 
 set +ve
