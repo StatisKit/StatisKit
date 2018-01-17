183a7a47b0364b4b53acd0eabe34da4fa49295df
Pierre Fernique <pfernique@gmail.com>
2018-01-17 15:56:54 +0100

    Update scripts

--- bin/script/linux/deploy.sh
+++ bin/script/linux/deploy.sh
@@ -1,5 +1,7 @@
 set -ve
 
+rm -rf ${CONDA_PREFIX}/conda-bld/src_cache
+rm -rf ${CONDA_PREFIX}/conda-bld/broken
 for filename in $(find ${CONDA_PREFIX}/conda-bld/ -name '*.tar.bz2')
 do
     anaconda upload ${filename} -u statiskit --label linux-release
--- bin/script/win/deploy.bat
+++ bin/script/win/deploy.bat
@@ -1,5 +1,7 @@
 echo ON
 
+rmdir %CONDA_PREFIX%\conda-bld\src_cache
+rmdir %CONDA_PREFIX%\conda-bld\broken
 for /R %%i in ("%CONDA_PREFIX%\conda-bld\*.tar.bz2") do (
     anaconda upload %%i -u %ANACONDA_UPLOAD% --label win-release
     if errorlevel 1 exit 1
