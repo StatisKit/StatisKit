caceb2b851d007d8fe03e14d169add1c1f7a1f2e
Pierre Fernique <pfernique@gmail.com>
2018-01-18 09:10:15 +0100

    Update scrips

--- bin/script/linux/deploy.sh
+++ bin/script/linux/deploy.sh
@@ -1,7 +1,9 @@
 set -ve
 
+source activate
 rm -rf ${CONDA_PREFIX}/conda-bld/src_cache
 rm -rf ${CONDA_PREFIX}/conda-bld/broken
+
 for filename in $(find ${CONDA_PREFIX}/conda-bld/ -name '*.tar.bz2')
 do
     anaconda upload ${filename} -u statiskit --label linux-release
--- bin/script/win/release.bat
+++ bin/script/win/release.bat
@@ -1,33 +1,70 @@
 echo ON
 
+set PY2K=2.7
+set PY3K=3.6
+
+rmdir %CONDA_PREFIX%\conda-bld /S /q
+
 cd ..\..\bin\conda
 if errorlevel 1 exit 1
-conda build python-scons ^
-            scons-tools ^
-            libtoolchain ^
-            python-toolchain ^
-            boost-suite ^
-            boost-meta ^
-            python-parse
+conda build python-scons --python=%PY2K%
+if errorlevel 1 exit 1
+conda build python-scons --python=%PY3K%
+if errorlevel 1 exit 1
+conda build scons-tools --python=%PY2K%
+if errorlevel 1 exit 1
+conda build scons-tools --python=%PY3K%
+if errorlevel 1 exit 1
+conda build libtoolchain --python=%PY2K%
+if errorlevel 1 exit 1
+conda build libtoolchain --python=%PY3K%
+if errorlevel 1 exit 1
+conda build python-toolchain --python=%PY2K%
+if errorlevel 1 exit 1
+conda build python-toolchain --python=%PY3K%
+if errorlevel 1 exit 1
+conda build boost-suite --python=%PY2K%
+if errorlevel 1 exit 1
+conda build boost-suite --python=%PY3K%
+if errorlevel 1 exit 1
+conda build boost-meta --python=%PY2K%
+if errorlevel 1 exit 1
+conda build boost-meta --python=%PY3K%
+if errorlevel 1 exit 1
+conda build python-parse --python=%PY2K%
+if errorlevel 1 exit 1
+conda build python-parse --python=%PY3K%
 if errorlevel 1 exit 1
 
 cd ..\..\share\git\ClangLite\bin\conda
 if errorlevel 1 exit 1
-conda build llvm ^
-            clang ^
-            libclanglite ^
-            python-clanglite
+conda build llvm
+if errorlevel 1 exit 1
+conda build clang
+if errorlevel 1 exit 1
+conda build libclanglite --python=%PY2K%
+if errorlevel 1 exit 1
+conda build libclanglite --python=%PY3K%
+if errorlevel 1 exit 1
+conda build python-clanglite --python=%PY2K%
+if errorlevel 1 exit 1
+conda build python-clanglite --python=%PY3K%
 if errorlevel 1 exit 1
 
 cd ..\..\..\AutoWIG\bin\conda
 if errorlevel 1 exit 1
-conda build python-autowig
+conda build python-autowig --python=%PY2K%
+if errorlevel 1 exit 1
+conda build python-autowig --python=%PY3K%
 if errorlevel 1 exit 1
 
 cd ..\..\..\..\..\bin\conda
 if errorlevel 1 exit 1
 
-conda build statiskit-dev
+conda build statiskit-dev --python=%PY2K%
+if errorlevel 1 exit 1
+conda build statiskit-dev --python=%PY3K%
+if errorlevel 1 exit 1
 
 cd ..\script\win
 
