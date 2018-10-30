6129e6b947cb987966fc286d2c6a5ee7faada145
Pierre <pfernique@gmail.com>
2018-10-30 15:50:02 +0100

    Update Boost recipe for windows

--- bin/conda/boost-suite/bld.bat
+++ bin/conda/boost-suite/bld.bat
@@ -71,7 +71,7 @@ if errorlevel 1 exit 1
 move %LIBRARY_LIB%\boost_*.dll "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
 
-move %SRC_DIR%\\Library mkdir %SRC_DIR%\
+move %SRC_DIR%\\Library %SRC_DIR%\
 if errorlevel 1 exit 1
 
 echo OFF
